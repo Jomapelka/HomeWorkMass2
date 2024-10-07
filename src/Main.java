bd663f3a735bfeb4c498fedf077ef59b0a41368b
Jomapelka <Belyashnoe.narezalovo@gmail.com>
2024-10-07 12:41:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] mass = new int[12];
+
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = i + 1;
+            System.out.println(mass[i]);
+        }
     }
 }
